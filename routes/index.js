8ec056c0dcc87e1b4a63a36e061a79ea7da6b055
YJC <openopentw@gmail.com>
2018-06-15 20:46:56 +0800

    remove client end so that it wont end !!!

--- routes/index.js
+++ routes/index.js
@@ -13,80 +13,25 @@ const client = new Client({
 })
 client.connect()
 
-// router.use(express.static(path.join('client/build')));
-router.use(express.static(path.join(__dirname, 'client/build')));
+router.use(express.static(path.join('client/build')));
+// router.use(express.static(path.join(__dirname, 'client/build')));
 
 const username = 'abc123'
 const hashedPassword = '$2b$10$1sSpo.bnaIfzQNw5Myh4AOJoujzjqKwn9xXz4RYIpRFu7MWulj9by'
 
-let list = [
-  {
-    article_id: 0,
-    article_title: 'art #1',
-    article_author: 'auth #1',
-    article_img: './IMG.png',
-  },
-  {
-    article_id: 1,
-    article_title: 'art #2',
-    article_author: 'auth #2',
-    article_img: './IMG.png',
-  },
-  {
-    article_id: 2,
-    article_title: 'art #3',
-    article_author: 'auth #3',
-    article_img: './IMG.png',
-  },
-  {
-    article_id: 3,
-    article_title: 'art #4',
-    article_author: 'auth #4',
-    article_img: './IMG.png',
-  },
-]
-
-let article = [
-  {
-    article_id: 0,
-    article_title: 'art #0',
-    article_author: 'auth #0',
-    article_img: './IMG.png',
-    article_content: 'aaaaaaaa\nbbbbbbb',
-  },
-]
-
 router.get('/api/list', function (req, res, next) {
-  // res.json(list);
-
   client.query('SELECT article_id, article_title, article_author, article_img FROM article;', (err, data) => {
     if (err) throw err;
-
-    for (let row of data.rows) {
-      console.log(JSON.stringify(row))
-    }
-
     res.json(data.rows)
-    client.end()
   })
-
 });
 
 router.get('/api/article/:artId', function (req, res, next) {
   let artId = parseInt(req.params.artId, 10)
-  console.log('artcie id: ' + artId)
-  client.query('SELECT * FROM article WHERE article_id = $1;', [artId.toString()], (err, data) => {
+  client.query(`SELECT * FROM article WHERE article_id = ${artId.toString()}`, (err, data) => {
     if (err) throw err;
-
-    for (let row of data.rows) {
-      console.log(JSON.stringify(row))
-    }
-
     res.json(data.rows[0])
-    client.end()
   })
-
-  // res.json(article[artId]);
 });
 
 router.post('/api/login', async function (req, res, next) {
